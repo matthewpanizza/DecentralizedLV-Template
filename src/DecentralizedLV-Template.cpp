58d813682a0572a0d495643f0cd04e546307b52f
Matthew Panizza <mlights999@gmail.com>
2024-09-14 20:04:19 -0400

    first commit

---
+++ src/DecentralizedLV-Template.cpp
@@ -0,0 +1,43 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#line 1 "c:/Users/mligh/OneDrive/Particle/DecentralizedLV-Template/src/DecentralizedLV-Template.ino"
+/* 
+ * Project myProject
+ * Author: Your Name
+ * Date: 
+ * For comprehensive documentation and examples, please visit:
+ * https://docs.particle.io/firmware/best-practices/firmware-template/
+ */
+
+// Include Particle Device OS APIs
+#include "Particle.h"
+
+// Let Device OS manage the connection to the Particle Cloud
+void setup();
+void loop();
+#line 13 "c:/Users/mligh/OneDrive/Particle/DecentralizedLV-Template/src/DecentralizedLV-Template.ino"
+SYSTEM_MODE(AUTOMATIC);
+
+// Run the application and system concurrently in separate threads
+SYSTEM_THREAD(ENABLED);
+
+// Show system, cloud connectivity, and application logs over USB
+// View logs with CLI using 'particle serial monitor --follow'
+SerialLogHandler logHandler(LOG_LEVEL_INFO);
+
+// setup() runs once, when the device is first turned on
+void setup() {
+  // Put initialization like pinMode and begin functions here
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+  // Example: Publish event to cloud every 10 seconds. Uncomment the next 3 lines to try it!
+  // Log.info("Sending Hello World to the cloud!");
+  // Particle.publish("Hello world!");
+  // delay( 10 * 1000 ); // milliseconds and blocking - see docs for more info!
+}
---
+++ src/DecentralizedLV-Template.ino
@@ -0,0 +1,25 @@
+/* 
+ * Project DecentralizedLV-Template
+ * Author: Your Name
+ * Date: 
+ * For comprehensive documentation and examples, please visit:
+ * https://docs.particle.io/firmware/best-practices/firmware-template/
+ */
+
+// Include Particle Device OS APIs
+#include "Particle.h"
+
+// Let Device OS manage the connection to the Particle Cloud
+SYSTEM_MODE(SEMI_AUTOMATIC);
+
+// setup() runs once, when the device is first turned on
+void setup() {
+  // Put initialization like pinMode and begin functions here
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+
+}
